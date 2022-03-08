05fe8b28f78b8655b93ed28c478d01d24994c843
ViktorSedov60 <98118779+ViktorSedov60@users.noreply.github.com>
2022-03-08 22:01:29 +0300

    Задание 13.8.19

---
+++ task_13.8.19.py
@@ -0,0 +1,11 @@
+N = int(input("ведите необходимое колличество билетов - "))
+result = 0
+
+
+for i in range(1, N+1):
+    age = int(input(f"ведите возраст посетителя номер {i} - "))
+    if 18 <= age < 25:
+      result += 990
+    elif age >= 25:
+      result += 1390
+print(f"сумма к оплате - {result}  рублей")
